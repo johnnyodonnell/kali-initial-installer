7914fdfdafd19ce6136b0ddfabb0f51625fea402
Johnny <odonnell_johnny@yahoo.com>
2024-06-08 15:06:59 -0700

    Update install.sh
    Try without full upgrade

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@
 # From https://www.kali.org/docs/general-use/metapackages/
 
 sudo apt update
-sudo apt full-upgrade -y
 
 sudo apt install -y kali-linux-headless
 sudo apt install -y kali-linux-wsl
