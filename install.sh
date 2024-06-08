186dae64cfe19cbfdcf8f88eec3c95044a9d93f8
Johnny <odonnell_johnny@yahoo.com>
2024-06-08 15:34:18 -0700

    Update install.sh
    Install headless last

--- install.sh
+++ install.sh
@@ -3,7 +3,8 @@
 # From https://www.kali.org/docs/general-use/metapackages/
 
 sudo apt update
+sudo apt full-upgrade -y
 
-sudo apt install -y kali-linux-headless
+sudo apt install -y kali-linux-core
 sudo apt install -y kali-linux-wsl
-
+sudo apt install -y kali-linux-headless
