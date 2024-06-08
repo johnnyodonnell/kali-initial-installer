9a86e400399c4bedeac9c9b45791a6383f8e6e60
Johnny O'Donnell <odonnell_johnny@yahoo.com>
2024-06-08 15:44:46 -0700

    Reformat comments

--- install.sh
+++ install.sh
@@ -2,9 +2,10 @@
 
 # From https://www.kali.org/docs/general-use/metapackages/
 
-# `sudo apt install -y kali-linux-core` might need to be run before `sudo apt full-upgrade -y`
-# Last time I installed Kali on WSL (6/8/2024) I had a lot of issue, and I finally
-# got it to work by installing `kali-linux-headless` last and installing `...-core` before
+# `sudo apt install -y kali-linux-core` might need to be run before
+# `sudo apt full-upgrade -y`. Last time I installed Kali on WSL (6/8/2024)
+# I had a lot of issue, and I finally got it to work by installing
+# `kali-linux-headless` last and installing `...-core` before
 # doing the `full-upgrade`
 
 sudo apt update
@@ -13,3 +14,4 @@ sudo apt full-upgrade -y
 sudo apt install -y kali-linux-core
 sudo apt install -y kali-linux-wsl
 sudo apt install -y kali-linux-headless
+
